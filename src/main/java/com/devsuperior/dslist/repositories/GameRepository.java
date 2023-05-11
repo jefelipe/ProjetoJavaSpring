2483f572c8ec6bda61b4c913bf30ac069acc4c0f
JeanFelipe <ermelindo35@hotmail.com>
2023-05-10 23:29:50 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dslist.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
 
